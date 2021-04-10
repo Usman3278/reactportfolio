52814ac2548b5ebf688884f7cfea939aca8ed27d
Usman3278 <usman3278@gmail.com>
2021-04-10 11:31:32 -0400

    Updates

---
+++ precache-manifest.b4d06ae4ea6b8f2819d10cd68483a7b8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de7a99cbb2c0a2d43f1351e42e31c57",
+    "url": "/Usman3278/reactportfolio/index.html"
+  },
+  {
+    "revision": "bd9249b7b12417afb504",
+    "url": "/Usman3278/reactportfolio/static/css/main.1dc9b250.chunk.css"
+  },
+  {
+    "revision": "158838e53688165f7baa",
+    "url": "/Usman3278/reactportfolio/static/js/2.4d5415ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Usman3278/reactportfolio/static/js/2.4d5415ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd9249b7b12417afb504",
+    "url": "/Usman3278/reactportfolio/static/js/main.e80dcc61.chunk.js"
+  },
+  {
+    "revision": "5a98ce3b28f0f0276ccc",
+    "url": "/Usman3278/reactportfolio/static/js/runtime-main.d9163355.js"
+  },
+  {
+    "revision": "f3dbd8226a7c733c412b2b7f4dbf306c",
+    "url": "/Usman3278/reactportfolio/static/media/BugTesters.f3dbd822.png"
+  },
+  {
+    "revision": "ea61edbc0b3c86c5531fd3a28dbb0dff",
+    "url": "/Usman3278/reactportfolio/static/media/BurgersDevoured.ea61edbc.png"
+  },
+  {
+    "revision": "2176540cdc57db83c6b498c22476ea41",
+    "url": "/Usman3278/reactportfolio/static/media/CodingQuiz.2176540c.png"
+  },
+  {
+    "revision": "b636056c6ba6bbe9109187a40c22ffdb",
+    "url": "/Usman3278/reactportfolio/static/media/DayPlanner.b636056c.png"
+  },
+  {
+    "revision": "80df4f6f467b96fe506acc046c6e8d53",
+    "url": "/Usman3278/reactportfolio/static/media/EmployeeDirectory.80df4f6f.png"
+  },
+  {
+    "revision": "81fd8b3f5dc64a50d5f979afc5bb62c6",
+    "url": "/Usman3278/reactportfolio/static/media/FitnessTracker.81fd8b3f.png"
+  },
+  {
+    "revision": "a4409b9592a7baacb71e627a29a52256",
+    "url": "/Usman3278/reactportfolio/static/media/MyResume.a4409b95.pdf"
+  },
+  {
+    "revision": "bf7f2ecd908fcff3edfab1c47439cef4",
+    "url": "/Usman3278/reactportfolio/static/media/NoteTaker.bf7f2ecd.png"
+  },
+  {
+    "revision": "3c9937950175adda4c7b76e61336172f",
+    "url": "/Usman3278/reactportfolio/static/media/PasswordGenerator.3c993795.png"
+  },
+  {
+    "revision": "711d68ea139617cc848a2a30bf19e2dc",
+    "url": "/Usman3278/reactportfolio/static/media/Profilepic.711d68ea.png"
+  },
+  {
+    "revision": "39f86a58c4b77bc548ddcfcd9ffbdfcd",
+    "url": "/Usman3278/reactportfolio/static/media/TravelGuideWebsite.39f86a58.png"
+  },
+  {
+    "revision": "0bae6e59e9ac69c7a19c8750676be54d",
+    "url": "/Usman3278/reactportfolio/static/media/background4.0bae6e59.png"
+  }
+]);
